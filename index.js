d34817399e04c254b8584c01357e7f7589bfa8a8
karissa <krmckelv@gmail.com>
2016-12-13 13:49:07 -0800

    make sure callback is checked first

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ module.exports = function townshipToken (maindb, options) {
   * @param {function} callback
   **/
   tokens.verify = function verify (token, options, callback) {
+    assert.equal(typeof callback, 'function', 'township-token: callback function is required')
     if (!(typeof token === 'string')) return callback(new Error('township-token: token parameter must be a string'))
 
     if (typeof options === 'function') {
@@ -113,8 +114,8 @@ module.exports = function townshipToken (maindb, options) {
   * @param {function} callback
   **/
   tokens.invalidate = function invalidate (token, callback) {
-    if (!(typeof token === 'string')) return callback(new Error('township-token: token parameter must be a string'))
     assert.equal(typeof callback, 'function', 'township-token: callback function is required')
+    if (!(typeof token === 'string')) return callback(new Error('township-token: token parameter must be a string'))
     db.put(token, token, callback)
   }
 
